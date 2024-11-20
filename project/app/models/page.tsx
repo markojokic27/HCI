18d6184ec3da86f6a02dfce915531cb97ccefcbb
markojokic27 <jokicmarko98@gmail.com>
2024-11-20 23:37:51 +0100

    pages added

---
+++ project/app/models/page.tsx
@@ -0,0 +1,18 @@
+// Components
+import { Layout, LayoutRow, LayoutColumn } from "@/components/Layout";
+import { Header } from "@/components/Header";
+
+export default function Page() {
+  return (
+    <>
+      <Header headerTheme="dark" />
+      <Layout className="mt-28">
+        <LayoutRow>
+          <LayoutColumn>
+            <h1 className="text-5xl text-center">Models</h1>
+          </LayoutColumn>
+        </LayoutRow>
+      </Layout>
+    </>
+  );
+}
